f05aa641aee3ff680904ab798e891bef165e42f1
2doh <ldh0973@naver.com>
2024-12-30 13:12:03 +0900

    docs : 프로젝트 기초 세팅

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>123</div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  methods: {},
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style></style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+// import 를 이용한 css 불러들이기
+// import "./assets/common.css";
+createApp(App).mount("#app");
